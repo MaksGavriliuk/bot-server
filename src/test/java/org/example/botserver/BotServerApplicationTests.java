3a78c49a00995f69f3ef682c7a9ccf9747982374
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-20 22:51:36 +0300

    create: model User

---
+++ src/test/java/org/example/botserver/BotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.botserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
