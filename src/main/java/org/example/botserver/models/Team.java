06252571a51b3bcfa3cf42c2b57663251aadbb19
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-20 22:58:10 +0300

    create: model Team

--- src/main/java/org/example/botserver/models/Team.java
+++ src/main/java/org/example/botserver/models/Team.java
@@ -1,26 +1,41 @@
 package org.example.botserver.models;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "teams")
+public class Team {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<User> users;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User leader;
 
 }
