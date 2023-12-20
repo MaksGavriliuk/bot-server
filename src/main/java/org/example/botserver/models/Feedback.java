20e6f512b412c6b8ca0e8312f8da9940123ab3c4
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-20 23:00:21 +0300

    create: model Feedback

--- src/main/java/org/example/botserver/models/Feedback.java
+++ src/main/java/org/example/botserver/models/Feedback.java
@@ -1,32 +1,29 @@
 package org.example.botserver.models;
 
-
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 
 @Data
+@Entity
+@Table(name = "feedbacks")
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Entity
-@Table(name = "users")
-public class User {
+public class Feedback {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "id")
+    private int id;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Feedback> feedbacks;
+    @Column(name = "feedback", columnDefinition = "TEXT")
+    private String feedback;
 
 }
