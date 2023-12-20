da5eb0fe06a4719104b6e7c04e094e9fb7e1e4a4
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-20 23:02:01 +0300

    create: model Category

---
+++ src/main/java/org/example/botserver/models/Category.java
@@ -0,0 +1,29 @@
+package org.example.botserver.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+@Data
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "category")
+    private String category;
+
+}
